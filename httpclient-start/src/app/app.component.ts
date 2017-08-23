9a96f4728c9507a46c1f1ef215c2fecec6c86be5
Austin Walker <awalker@crossroadstech.net>
2017-08-23 08:48:31 -0600

    httpClient practice

---
+++ httpclient-start/src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase';
+import { config } from './config';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  loadedFeature = 'recipe';
+
+  ngOnInit() {
+    firebase.initializeApp({
+      apiKey: config.apiKey,
+      authDomain: config.authDomain
+    });
+  }
+
+  onNavigate(feature: string) {
+    this.loadedFeature = feature;
+  }
+}
